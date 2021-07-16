7035b3d6d20e1bd46c406bb041543cb9c1a9ca92
satikaramadhani <87509705+satikaramadhani@users.noreply.github.com>
2021-07-15 20:51:01 -0700

    program konversi dari kilo ke mil

---
+++ KiloKeMil.java
@@ -0,0 +1,11 @@
+public class KiloKeMil {
+    public static void main(String[] args) {
+
+        double km = 79;
+        double mil;
+        mil = 0.621371 * km;
+
+        System.out.println("Nilai Kilometer = " + km);
+        System.out.println("Nilai Mil = " + mil);
+    }
+}
